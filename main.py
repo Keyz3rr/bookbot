9a33fd58f33246bba38d765a5018d060105c1638
Keyz3rr <toreriklauv@gmail.com>
2025-05-29 21:10:45 +0200

    finished the bookbot

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from stats import get_word_count
+from stats import get_word_count, get_num_characters, sort_char_count
 
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+import sys
+
+def get_book_text(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
-    file_contents = get_book_text()
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    file_contents = get_book_text(book_path)
     num_words = get_word_count(file_contents)
-    print(f"{num_words} words found in the document")
+    character_count = get_num_characters(file_contents)
+    characters = sort_char_count(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in characters:
+        char_key = char["char"]
+        char_count = char["num"]
+        print(f"{char_key}: {char_count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,28 @@
 def get_word_count(file_contents):
-        word_count = file_contents.split()
-        num_words = 0
-        for word in word_count:
-            num_words += 1
-        return num_words
+    word_count = file_contents.split()
+    num_words = 0
+    for word in word_count:
+        num_words += 1
+    return num_words
+
+def get_num_characters(file_contents):
+    character_count = {}
+    for character in file_contents:
+        character = character.lower()
+        if character in character_count:
+            character_count[character] = character_count[character] + 1
+        else:
+            character_count[character] = 1
+    return character_count
+
+def sort_on(character_count):
+    return character_count["num"]
+
+def sort_char_count(character_count):
+    characters = []
+    for c in character_count:
+        if c.isalpha():
+            c_count = {"char": c, "num": character_count[c]}
+            characters.append(c_count)
+    characters.sort(reverse=True, key=sort_on)
+    return characters
